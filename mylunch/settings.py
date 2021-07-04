b327775d7aaeac825e253b1c72aebb0c32427c1a
Mohammad Jakir Hossain <mjakir.ddd@gmail.com>
2021-07-05 01:54:48 +0600

    Configure restframework and djoser in settings.py

--- mylunch/settings.py
+++ mylunch/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party app
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+
+    # Project app
     'core',
 ]
 
@@ -128,6 +135,18 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
 
 # LOGGING SETTINGS
 LOGGING = {
